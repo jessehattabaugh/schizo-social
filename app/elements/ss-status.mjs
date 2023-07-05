faa3d5f8a5fc924abcc5397d5ef93371eec1e3ff
Jesse Hattabaugh <jessehattabaugh@protonmail.com>
2023-07-04 18:38:28 -0700

    display reblogs

--- app/elements/ss-status.mjs
+++ app/elements/ss-status.mjs
@@ -5,6 +5,22 @@ export default function ({ html, state }) {
 	const statusIds = Object.keys(statuses);
 	const status = statuses[status_id];
 	console.debug('🛻', { status_id, statusIds, status });
+	const {
+		content,
+		created_at,
+		media_attachments,
+		spoiler_text,
+		url: status_url,
+	} = status.reblog || status;
+	const {
+		avatar,
+		display_name,
+		header,
+		note,
+		url: account_url,
+		username,
+	} = status.reblog?.account || status.account;
+	const reblogger = status.reblog && status.account;
 	return (
 		status &&
 		html`<style>
@@ -67,28 +83,28 @@ export default function ({ html, state }) {
 			</style>
 			<article class="h-entry">
 				<header class="h-card">
-					<details style="background-image: url('${status.account.header}')">
+					<details style="background-image: url('${header}')">
 						<summary>
 							<img
-								alt="${status.account.display_name}"
+								alt="${display_name}"
 								class="u-photo"
 								fetchpriority="high"
 								height="10%"
 								loading="lazy"
-								src="${status.account.avatar}"
+								src="${avatar}"
 								width="10%"
 							/>
-							<h3 class="p-name">${status.account.display_name}</h3>
+							<h3 class="p-name">${display_name}</h3>
 						</summary>
-						<div class="note">${status.account.note}</div>
+						<div class="note">${note}</div>
 					</details>
 				</header>
-				${status.spoiler_text &&
+				${spoiler_text &&
 				`<section class="spoiler_text">
-		${status.spoiler_text}
-	</section>`}
-				<section class="e-content">${status.content}</section>
-				${status.media_attachments
+					${spoiler_text}
+				</section>`}
+				<section class="e-content">${content}</section>
+				${media_attachments
 					.map(
 						(
 							/** @type {{ type: string; url: any; description: any; preview_url: any; }} */ attachment,
@@ -96,27 +112,29 @@ export default function ({ html, state }) {
 							return (
 								attachment.type == 'image' &&
 								`<picture>
-					<source
-						media="(min-width: 600px)"
-						srcset="${attachment.url}" />
-					<img
-						alt="${attachment.description || 'no description'}"
-						class="attachment"
-						loading="lazy"
-						src="${attachment.preview_url}"
-						width="100%"
-				/></picture>`
+									<source
+										media="(min-width: 600px)"
+										srcset="${attachment.url}" />
+									<img
+										alt="${attachment.description || 'no description'}"
+										class="attachment"
+										loading="lazy"
+										src="${attachment.preview_url}"
+										width="100%"
+								/></picture>`
 							);
 						},
 					)
 					.join('\n')}
 				<h5>
-					<a class="p-author u-url" href="${status.account.url}">
-						^ by ${status.account.username}
-					</a>
-					<a class="u-url" href="${status.url}" style="text-align: right;">
-						<time class="dt-published" datetime="${status.created_at}">
-							at ${new Date(status.created_at).toLocaleString()}</time
+					<a class="p-author u-url" href="${account_url}"> ^ by ${username} </a>
+					${reblogger &&
+					`<a class="p-author u-url" href="${reblogger.url}">
+						&lt; reblogged by ${reblogger.username}
+					</a>`}
+					<a class="u-url" href="${status_url}" style="text-align: right;">
+						<time class="dt-published" datetime="${created_at}">
+							at ${new Date(created_at).toLocaleString()}</time
 						>
 					</a>
 				</h5>
