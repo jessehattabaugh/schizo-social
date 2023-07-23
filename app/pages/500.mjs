087967918e0fe1665449dacf9914883e41ca9a70
Jesse Hattabaugh <jessehattabaugh@protonmail.com>
2023-07-22 22:06:00 -0700

    update error pages

--- app/pages/500.mjs
+++ app/pages/500.mjs
@@ -2,8 +2,11 @@
 export default function ({ html, state }) {
 	const { error } = state.attrs;
 	return html`<main>
-		<h1>404 Not Found</h1>
-		<h2>The server cannot find the requested resource.</h2>
+		<h1>500 Internal Server Error</h1>
+		<h2>
+			The server encountered an unexpected condition that prevented it from fulfilling the
+			request.
+		</h2>
 		<p>${error && error}</p>
 	</main>`;
 }
