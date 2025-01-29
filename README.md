c631c8e70500a0d341077bd147b7bcfb3815511c
Jesse Hattabaugh <jessehattabaugh@protonmail.com>
2025-01-28 18:59:44 -0800

    my first CDK app

---
+++ README.md
@@ -0,0 +1,11 @@
+# schizo.social
+
+schizo.social is a social media platform that allows users to track the accuracy of predictions made in news articles about financial markets.
+
+## Development
+
+To preview the static site, run `npm run start`.
+
+## Deployment
+
+To deploy the static site you must first [install the AWS CLI](https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html) and [configure AWS credentials](https://docs.aws.amazon.com/cli/v1/userguide/cli-configure-files.html). Then run `npm run bootstrap` once, and `npm run deploy` each time you want to deploy.
---
+++ app.js
@@ -0,0 +1,41 @@
+/**
+ * @file deploys this site
+ * @see https://docs.aws.amazon.com/cdk/api/v2/
+ */
+
+import {App, RemovalPolicy, Stack} from 'aws-cdk-lib';
+import { Bucket, BlockPublicAccess } from 'aws-cdk-lib/aws-s3';
+import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
+
+/**
+ * A stack that deploys a website to an S3 bucket.
+ * @extends Stack
+ * @param {import('constructs').Construct} scope The parent construct.
+ * @param {string} id An identifier for this construct.
+ * @param {import('aws-cdk-lib').StackProps} [props] Stack properties.
+ */
+class WWWStack extends Stack {
+	constructor(scope, id, props) {
+		super(scope, id, props);
+
+		// Create an S3 bucket that hosts a website
+		const destinationBucket = new Bucket(this, 'WWWBucket', {
+			websiteIndexDocument: 'index.html',
+			blockPublicAccess: BlockPublicAccess.NONE,
+
+			// Optionally, remove the bucket on stack removal
+			removalPolicy: RemovalPolicy.DESTROY,
+			autoDeleteObjects: true,
+		});
+
+		// Deploy files in ./www to the bucket
+		new BucketDeployment(this, 'WWWDeployment', {
+			sources: [Source.asset('./www')],
+			destinationBucket,
+		});
+	}
+}
+
+// Create the CDK app and stack
+const app = new App();
+new WWWStack(app, 'WWWStack');
